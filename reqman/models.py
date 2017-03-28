b9cd25f0364605f69ce24aef7a81e44e94846e19
MTurgeon <maxime.turgeon@mail.mcgill.ca>
2017-03-28 15:16:33 -0600

    define model attributes

--- reqman/models.py
+++ reqman/models.py
@@ -2,4 +2,37 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Analyst(models.Model):
+    # Attributes
+    name = models.CharField(max_length = 30)
+    email = models.CharField(max_length = 30)
+    phone_number = models.CharField(max_length = 30)
+
+class Database(models.Model):
+    # Attributes
+    name = models.CharField(max_length = 30)
+    location = models.CharField(max_length = 30)
+    connection_details = models.CharField(max_length = 30)
+    maintained_stakeholder = models.BooleanField()
+
+class Stakeholder(models.Model):
+    # Attributes
+    name = models.CharField(max_length = 30)
+    title = models.CharField(max_length = 30)
+    department = models.CharField(max_length = 30)
+
+class Request(models.Model):
+    # Attributes
+    title = models.CharField(max_length = 30)
+    date_created = models.DateField()
+    date_requested = models.DateField()
+    date_released = models.DateField()
+    stakeholder = models.ForeignKey(Stakeholder, on_delete = models.CASCADE)
+    analysts = models.ManyToManyField(Analyst, through = 'Contribution')
+    databases = models.ManyToManyField(Database)
+
+class Contribution(models.Model):
+    # Attributes
+    request = models.ForeignKey(Request, on_delete = models.CASCADE)
+    analyst = models.ForeignKey(Analyst, on_delete = models.CASCADE)
+    main_analyst = models.BooleanField()
--- reqman/views.py
+++ reqman/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse("This is the home page of the app. This is where the entry form will be.")
